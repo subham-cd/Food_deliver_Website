8516676c146f4e84c4b8277c6f3feb06cb0d7930
Subham Singh <subham0506singh@gmail.com>
2025-06-11 22:49:52 +0530

    Frontend work ongoing

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import React from "react"
+import Navbar from "./components/Navbar/Navbar"
+import { Route, Routes } from "react-router-dom"
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+import Home from "./pages/Home/Home.jsx"
+
+
+function App() {
+
+  return (
+   <div className="app">
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/order" element={<PlaceOrder/>}/>
+    
+
+     
+    </Routes>
+
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingredients and culinary expertise.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
